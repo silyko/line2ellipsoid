78032f2e85f8d43dbf422d932005aa032caa0c19
Simon Kokkendorff <sik@dmi.dk>
2016-01-04 13:42:25 +0100

    fixed an orientation issue

--- line2ellipsoid.py
+++ line2ellipsoid.py
@@ -3,7 +3,10 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-DEBUG = True
+import logging
+
+LOG = logging.getLogger(__name__)
+
 colors = ["red", "blue", "green"]
 
 
@@ -37,13 +40,15 @@ def project_onto_ellipse(x, y, w):
             if d < dmin or dmin is None:
                 p = q
                 dmin = d
+                LOG.debug("New min: %.3f, root: %s" % (d, q))
+    LOG.debug("Projection of x: %.2f, y: %.2f, is xp: %.2f yp: %.2f" % (x, y, np.real(p), np.imag(p)))
     return (np.real(p), np.imag(p)), dmin
     
 
 def project_onto_ellipse2(x, y, w):
     """Brute force projection onto an ellipse with axes w[0], w[1] of a point (x,y).
     Substitute with any better, well known, procedure!"""
-    print("Projecting x: %.6f, y: %.6f, x/y: %.6f" % (x,y, x/y))
+    LOG.debug("Projecting x: %.6f, y: %.6f, x/y: %.6f" % (x,y, x/y))
     theta = np.arctan2(y,x)
     delta = np.radians(5.0 * np.max(w) / np.min(w)) # Hmmm...
     iterations = 0
@@ -53,8 +58,8 @@ def project_onto_ellipse2(x, y, w):
     dist = None
     while dot <= last_dot and iterations < 20:
         last_dot = dot
-        print("Iteration: %d, Curent angle: %.6f, current dot: %.6f" % (iterations, np.degrees(theta), dot))
-        print("Delta: %.4f" % np.degrees(delta))
+        LOG.debug("Iteration: %d, Curent angle: %.6f, current dot: %.6f" % (iterations, np.degrees(theta), dot))
+        #print("Delta: %.4f" % np.degrees(delta))
         vs = np.linspace(theta - delta,theta + delta, 100000)
         ex = w[0]*np.cos(vs)
         ey = w[1]*np.sin(vs)
@@ -68,7 +73,7 @@ def project_onto_ellipse2(x, y, w):
             p = E[i]
             dot = dots[i]
             dist = np.sqrt(((p-(x,y))**2).sum())
-        print("DISTANCE is now: %.8f, dot: %.8f" % (dist, dot))
+        LOG.debug("DISTANCE is now: %.8f, dot: %.8f" % (dist, dot))
         iterations += 1
     return p, dist
 
@@ -114,7 +119,7 @@ def get_some_points_on_ellipsoid(a,b,c):
     ns = np.array(ns)
     return transform_to_ellipsoid(ns, a, b, c)
 
-def stuffit(a, b ,c, x1, x2):
+def stuffit(a, b ,c, x1, x2, debug=False):
     """Return a normal vector for the closest point, as well as distance.
     a,b,c are ellipsoidal axes, x1, x2 endpoints of a line.
     """
@@ -123,7 +128,6 @@ def stuffit(a, b ,c, x1, x2):
     line = np.vstack((x1,x2))
     # Hey, first get the normals
     n = normalise(x2 - x1)
-    print "norm is", (n**2).sum(axis=1)
     # We now have a nice normal vector...
     # Parametrise the plane orthogonal to that
     if abs(n[0,2]) > 0.2:
@@ -131,32 +135,35 @@ def stuffit(a, b ,c, x1, x2):
         e1 = n + np.array([[10, 0, 0]])
     else:
         e1 = n +  np.array([[0, 0, 10]])
-    if DEBUG:
-        print("Was: %s" % str(n))
-        print("Now: %s" % str(e1))
+    if debug:
+        LOG.debug("Was: %s" % str(n))
+        LOG.debug("Now: %s" % str(e1))
     e1 = normalise(project_onto_complement(n, e1))
-    if DEBUG:
-        print("in plane: %s" % str(e1))
+    if debug:
+        LOG.debug("in plane: %s" % str(e1))
+    # This fixes our orientation
     e2 = np.cross(n, e1)
     miss= np.fabs(e2 - project_onto_complement(n, e2))
     assert (np.fabs(miss) < 1e-5).all()
-    if DEBUG:
-        print("miss: %.6f" % miss.max())
-        print("Basis:")
-        print("e1: %s" % str(e1))
-        print("e2: %s" % str(e2))
-        print("e1 dot e2: %.7f " % (e1*e2).sum())
-        print("norm e2: %.7f" % (e2**2).sum())
-        print("norm e1: %.7f" % (e1**2).sum())
-        print("norm n: %.7f" % (n**2).sum())
-        print("normal: %s" % str(n))
+
+    LOG.debug("miss: %.6f" % miss.max())
+    LOG.debug("Basis:")
+    LOG.debug("e1: %s" % str(e1))
+    LOG.debug("e2: %s" % str(e2))
+    LOG.debug("e1 dot e2: %.7f " % (e1*e2).sum())
+    LOG.debug("norm e2: %.7f" % (e2**2).sum())
+    LOG.debug("norm e1: %.7f" % (e1**2).sum())
+    LOG.debug("norm n: %.7f" % (n**2).sum())
+    LOG.debug("normal: %s" % str(n))
     
     # Now find a basis for the 'in-between' plane, where points on 
     # ellipsoid, and normals, are parametrised by unit vectors
     te1 = transform_to_ellipsoid(e1, a, b, c)
     te2 = transform_to_ellipsoid(e2, a, b, c)
     te1, te2 = basis_to_onb(te1, te2)
-    if DEBUG:
+    det = (te2*np.cross(n,te1)).sum()
+    LOG.debug("WUP: %.2f" % (det))
+    if debug:
         # Get some points on the ellipsoid, if we wanna do some plotting
         ellipse = get_some_points_on_ellipsoid(a, b, c)
         # Parametrise some normals in that plane.
@@ -164,11 +171,10 @@ def stuffit(a, b ,c, x1, x2):
         ns = np.cos(vs)*te1 + np.sin(vs)*te2
         on_ellipsoid = transform_to_ellipsoid(ns, a, b, c)
         in_plane = project_onto_complement(n, on_ellipsoid)
-    if DEBUG:
-        print("ONB for TP:")
-        print("e1: %s, e2: %s" % (str(te1), str(te2)))
-        print("norms: %.6f %.6f, dot: %.6f" % ((te1**2).sum(), (te2**2).sum(), (te1*te2).sum()))
-    # The mapping e1 -> te1, e2-> te2 gives an isometric identification of the two planes.
+    
+    LOG.debug("ONB for TP:")
+    LOG.debug("e1: %s, e2: %s" % (str(te1), str(te2)))
+    LOG.debug("norms: %.6f %.6f, dot: %.6f" % ((te1**2).sum(), (te2**2).sum(), (te1*te2).sum()))
     # OK, find the matrix for the composite map ( to_ellipsoid -> project_onto_complement)
     # Going from T(P) to P.
     # We are using the basis for P determined above {e1, e2}, and {te1, te2} for T(P)
@@ -181,28 +187,37 @@ def stuffit(a, b ,c, x1, x2):
     a12 = (pe2*e1).sum() 
     a22 = (pe2*e2).sum()
     sym_dif = ((pe1*e2).sum() - (pe2*e1).sum())
-    print("IS SYMMETRIC: %.6f" % sym_dif)
+    LOG.debug("IS SYMMETRIC: %.6f" % sym_dif)
     # OK - so not symmetric...
     A = np.array(((a11,a12),(a21,a22)))
+    LOG.debug("Determinant of A: %.4f" % (np.linalg.det(A)))
     u, w, v = np.linalg.svd(A)
-    print u, "U"
-    print v, "V"
-    print("Singular values: %.7f, %.7f" % (w[0],w[1]))
-    # EIGENSPACE MIGHT HAVE MULTIPLICITY 2 (a sphere)
-    # So: just hat the first eigenvector, instead of using the computed one!
+    LOG.debug("Singular values: %.7f, %.7f" % (w[0],w[1]))
+    # AND: make sure we preserve orientation here
     # These are the eigenvectors expressed in the 'standard' basis
-    ee1 = u[0,0]*e1+u[1,0]*e2
-    ee2 = u[1,0]*e1-u[0,0]*e2
+    u1 = u[:,0]
+    u2 = u[:,1]
+    v1 = v[:,0]
+    v2 = v[:,1]
+    ee1 = u1[0]*e1+u1[1]*e2
+    ee2 = u2[0]*e1+u2[1]*e2
     # corresponding basis of TP:
-    tee1 = v[0,0]*te1+v[1,0]*te2
-    tee2 = v[1,0]*te1-v[0,0]*te2
-    if DEBUG:
-        print("NEW ONB:")
-        print("e1: %s" % str(ee1))
-        print("ee2: %s" % str(ee2))
-        print("DOTS: %.8f %.8f" % ((ee1*n).sum(), (ee2*n).sum()))
-        print("NORMS %.8f %.8f" % ( (ee1**2).sum(), (ee2**2).sum()))
-        print("ONB? %.8f" % (ee1*ee2).sum())
+    tee1 = v1[0]*te1+v1[1]*te2
+    tee2 = v2[0]*te1+v2[1]*te2
+    # linalg.svd seems to sometime reverse the orientation
+    det_u = np.linalg.det(u)
+    det_v = np.linalg.det(v)
+    det = (ee2*np.cross(n,ee1)).sum()
+    #if det_u < 0:
+    #    ee1 = -ee1
+    #    tee1 = -tee1
+    LOG.debug("Det U: %.2f, Det V: %.2f, det: %.2f" % (det_u, det_v, det))
+    LOG.debug("'Eigenbasis' for P, TP:")
+    LOG.debug("ee1: %s, tee1: %s" % (str(ee1), str(tee1)))
+    LOG.debug("ee2: %s, tee2: %s" % (str(ee2), str(tee2)))
+    LOG.debug("DOTS: %.8f %.8f" % ((ee1*n).sum(), (ee2*n).sum()))
+    LOG.debug("NORMS %.8f %.8f" % ( (ee1**2).sum(), (ee2**2).sum()))
+    LOG.debug("ONB? %.8f" % (ee1*ee2).sum())
     # Get the 'line point' in the orthogonal plane...
     L = project_onto_complement(n, x1)
     # Just checking...
@@ -213,7 +228,7 @@ def stuffit(a, b ,c, x1, x2):
     y = (ee2*L).sum()
     #Try to minimize - brute force
     p, dist = project_onto_ellipse(x, y, w)
-    if DEBUG:
+    if debug:
         # Just debugging
         xp = (in_plane * ee1).sum(axis=1)
         yp = (in_plane * ee2).sum(axis=1)
@@ -229,13 +244,21 @@ def stuffit(a, b ,c, x1, x2):
     n2 = p[1] / w[1]
     # We now use the identification of P and TP
     unit_vector = n1*tee1 + n2*tee2
+    # Sometimes we seem to get the antipodal vector, due to some orientation thing (in numpy?)
+    # Corresponds to multiplying either u or v with -1
+    normal = normalise(transform_to_ellipsoid(unit_vector, 1/a, 1/b, 1/c))
+    direc = (L*normal).sum()
+    LOG.debug("DIRECTION: %.4f" % direc)
+    if direc < 0:
+        unit_vector = - unit_vector
+    LOG.debug("Unit vector: %s" % unit_vector)
     assert abs(np.sqrt((unit_vector**2).sum()) - 1) < 1e-5
     final_point = transform_to_ellipsoid(unit_vector, a, b, c)
-    if DEBUG:
+    if debug:
         plot_in_3d([ellipse],[line],[final_point[0]])
     final_normal = normalise(transform_to_ellipsoid(unit_vector, 1/a, 1/b, 1/c))
     final_dot = (final_normal*n).sum()
-    print("FINAL DOT: %.6f" % final_dot)
+    LOG.debug("FINAL DOT: %.6f" % final_dot)
     return final_normal, dist
     
 
@@ -262,11 +285,14 @@ def normal_to_ellipsoid(n, a, b, c):
 
 
 if __name__ == "__main__":
-    lat = 10.82
-    lon = 55.17
-    d= 100.0
+    lon = 10.82
+    lat = 55.73
+    d= 400000.0
+    R1 = 6378137.000
+    R2 = 6356752.314140
+    logging.basicConfig(level=logging.DEBUG)
     print("Starting with dist: %.3f, lat: %.5f lon: %.5f" % (d, lat, lon))
-    a,b,c = 4000.0, 3600.0, 3000.0
+    a,b,c = R1, R1, R2
     n1 = latlon_to_normal(np.radians(lat), np.radians(lon))
     v= np.cross(n1,(-2,7,3))
     # just checking
@@ -274,9 +300,9 @@ if __name__ == "__main__":
     n2 = ellipsoidal_xyz_to_normals(xyz, a, b, c)
     assert np.fabs(n1-n2).max() < 1e-6
     p0 = xyz + d*n1
-    p1 = p0 + 1000.0*v
-    p2 = p0 - 1000.0*v
-    n, dist = stuffit(a, b, c, p1, p2)
+    p1 = p0 + a*0.5*v
+    p2 = p0 - a*0.5*v
+    n, dist = stuffit(a, b, c, p1, p2, True)
     print("p1: %s, p2: %s" % (str(p1), str(p2))) 
     print("Output normal: %s, dot-line: %.6f" % (n, ((p1 - p2)*n).sum()))
     lat, lon = np.degrees(normal_to_latlon(n))
